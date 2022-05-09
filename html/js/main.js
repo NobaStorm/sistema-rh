b15fc3ac2cc7d02376526c9fccc384dc799a06c4
Armando Martín Martínez león Martínez león <aleon7892@gmail.com>
2022-05-09 13:42:21 -0500

    Login, Main ok

---
+++ html/js/main.js
@@ -0,0 +1,74 @@
+
+window.onload = init;
+var headers = {}
+var url = 'http://localhost:3000';
+var employees_l = 0;
+
+function init() {
+    if(localStorage.getItem("token")){
+        headers = {
+            headers:{
+                'Authorization': "bearer " + localStorage.getItem("token")
+            }
+        }
+        loadEmployees();
+        document.querySelector('.btn-outline-primary').addEventListener('click', loadEmployeesName)
+    }
+    else{
+        window.location.href = "login2.html"
+    }
+}
+
+function loadEmployees(){
+    axios.get(url + "/empleados", headers).then(function(res){
+        console.log(res);
+        employees_l = res.data.message.length;
+        console.log(employees_l);
+        displayEmployees(res.data.message);
+        
+    }).catch(function(err){
+        console.log(err);
+    })
+}
+
+function loadEmployeesName(){
+    var nombre = document.getElementById('search').value;
+    if(nombre === ""){
+        loadEmployees();
+    }
+    axios.get(url + `/empleados/${nombre}`, headers).then(function(res){
+        console.log(res);
+        //removeAllEmployees();
+        var table_node = document.getElementsByTagName("table")[0];
+        table_node.innerHTML = `       <tr>\n` +
+        `            <th scope="col">Id</th>\n` +
+        `            <th scope="col">Nombre</th>\n` +
+        `            <th scope="col">Apellido</th>\n` +
+        `            <th scope="col">Teléfono</th>\n` +
+        `            <th scope="col">Correo</th>\n` +
+        `            <th scope="col">Dirección</th>\n` +
+        `            <th scope="col">Editar</th>\n` +
+        `            <th scope="col">Eliminar</th>\n` +
+        `        </tr>`;
+        displayEmployees(res.data.message);
+        
+    }).catch(function(err){
+        console.log(err);
+    })
+}
+
+function displayEmployees(employees){
+    var table_node = document.getElementsByTagName("table")[0];
+    for(var i=0; i < employees.length; i++){
+        table_node.innerHTML+= `       <tr>\n` +
+        `            <td>${employees[i].EMP_ID}</td>\n` +
+        `            <td>${employees[i].EMP_NAME}</td>\n` +
+        `            <td>${employees[i].EMP_LASTNAME}</td>\n` +
+        `            <td>${employees[i].EMP_PHONE}</td>\n` +
+        `            <td>${employees[i].EMP_EMAIL}</td>\n` +
+        `            <td>${employees[i].EMP_ADDRESS}</td>\n` +
+        `            <td><button class="btn btn-outline-primary" type="button">Editar</button></td>\n` +
+        `            <td><button class="btn btn-outline-danger" type="button">Eliminar</button></td>\n` +
+        `        </tr>`
+    }
+}
