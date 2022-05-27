226c73bf445105d7f9b283d5da8d40fd1ba3e01d
Armando Martín Martínez león Martínez león <aleon7892@gmail.com>
2022-05-27 12:21:14 -0500

    add DB azure

--- config/database.js
+++ config/database.js
@@ -3,9 +3,9 @@ const util = require('util');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: '',
+    host: 'sistemarhdb.mysql.database.azure.com',
+    user: 'kemonito@sistemarhdb',
+    password: 'Admin-rh',
     database: 'sistemarh'
 });
 
